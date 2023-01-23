8af1dfe9392c102ece0150bd7901aad1590a630f
Alex Chadwick <alex.chadwick@mindprofessionals.com>
2023-01-23 20:37:24 +0000

    changed env vars

--- src/lib/sb.ts
+++ src/lib/sb.ts
@@ -1,10 +1,10 @@
-import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-console.log(env);
-
-if (!env.PUBLIC_SUPABASE_URL || !env.PUBLIC_SUPABASE_ANON_KEY) {
+if (!import.meta.env.VITE_SUPABASE_URL || !import.meta.env.VITE_SUPABASE_ANON_KEY) {
 	throw new Error('Supabase public URL and anon key must be set in environment variables');
 }
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
+);
